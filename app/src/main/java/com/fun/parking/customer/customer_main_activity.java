e8b3dad1caae5a5eda9581618e23045f4491ca20
oravital7 <oravital7@gmail.com>
2019-11-13 21:00:28 +0200

    add customer btn

--- app/src/main/java/com/fun/parking/customer/customer_main_activity.java
+++ app/src/main/java/com/fun/parking/customer/customer_main_activity.java
@@ -3,6 +3,8 @@ package com.fun.parking.customer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.fun.parking.R;
 
@@ -12,5 +14,10 @@ public class customer_main_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customer_main_activity);
+    }
+
+    public void RentBtn(View v)
+    {
+        Toast.makeText(this, "RentBtn", Toast.LENGTH_SHORT).show();
     }
 }
