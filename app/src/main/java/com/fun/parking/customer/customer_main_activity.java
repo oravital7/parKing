7b1c1a4c7b89ba9bf0ad98f8633017df9235daa0
oravital7 <oravital7@gmail.com>
2019-11-13 13:42:37 +0200

    add Customer activity

---
+++ app/src/main/java/com/fun/parking/customer/customer_main_activity.java
@@ -0,0 +1,16 @@
+package com.fun.parking.customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fun.parking.R;
+
+public class customer_main_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer_main_activity);
+    }
+}
