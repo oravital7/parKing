4d33c718d1bf74ddcb91f289e95ff045729ece71
Dana Mor <dandimor@gmail.com>
2019-12-09 09:17:05 +0200

    main activity update

--- app/src/main/java/com/fun/parking/MainActivity.java
+++ app/src/main/java/com/fun/parking/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.fun.parking.business.BusinessMainActivity;
+
 import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,12 +20,19 @@ public class MainActivity extends AppCompatActivity {
         mRent= findViewById(R.id.rent);
         mFind = findViewById(R.id.findParking);
 
-//        mFind.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, buisness.BuisnessMainActivity.class));
-//            }
-//        });
+        mFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), BusinessMainActivity.class));
+            }
+        });
+
+        mRent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), com.fun.parking.customer.MainActivity.class));
+            }
+        });
 
 
 
