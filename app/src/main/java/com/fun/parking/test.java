05533c4dcc45fea227734cfca6f69c0b355fe481
Dana Mor <dandimor@gmail.com>
2019-12-09 19:45:23 +0200

    orfu

--- app/src/main/java/com/fun/parking/test.java
+++ app/src/main/java/com/fun/parking/test.java
@@ -18,35 +18,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class test extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
+    //private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-// Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-
-// Add a new document with a generated ID
-        db.collection("test")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
 
     }
 }
