63b47aa8d728f4a5ca5180148269daabfcf43e90
Dana Mor <dandimor@gmail.com>
2020-01-12 09:47:32 +0200

    user profile changes

--- app/src/main/java/com/fun/parking/UserProfile.java
+++ app/src/main/java/com/fun/parking/UserProfile.java
@@ -9,6 +9,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -38,17 +39,19 @@ import javax.annotation.Nullable;
 
 //public class UserProfile extends AppCompatActivity {
 public class UserProfile extends BaseActivity{
-    TextView fullName,email,phone,address;
+    TextView fullName,email,phone,address, changeImg;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userId;
     Button logout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_user_profile);
+        changeImg = findViewById(R.id.changeProfilePic);
         phone = findViewById(R.id.profilePhone);
         fullName = findViewById(R.id.profileName);
         email    = findViewById(R.id.profileEmail);
@@ -56,11 +59,10 @@ public class UserProfile extends BaseActivity{
         logout = findViewById(R.id.logout);
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
-
         userId = fAuth.getCurrentUser().getUid();
 
 
-    DocumentReference documentReference = fStore.collection("users").document(userId);
+        DocumentReference documentReference = fStore.collection("users").document(userId);
 
         documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -85,14 +87,32 @@ public class UserProfile extends BaseActivity{
             }
         });
 
+//        changeImg.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+//                // Sets the type as image/*. This ensures only components of type image are selected
+//                intent.setType("image/*");
+//                startActivityForResult(Intent.createChooser(intent, "pick an image"), requestCode 1);
+//            }
+//        });
+
+
     }
-
-
-
-
-
-
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode,int resultCode,Intent data)
+//    {
+//        // Result code is RESULT_OK only if the user selects an Image
+//        if (resultCode == RESULT_OK&&requestCode==1)
+//            switch (requestCode){
+//                case GALLERY_REQUEST_CODE:
+//                    //data.getData returns the content URI for the selected Image
+//                    Uri selectedImage = data.getData();
+//                    imageView.setImageURI(selectedImage);
+//                    break;
+//            }
+//    }
+}
 
 
 
